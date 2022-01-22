6c93a25b29fb2fbe337e38d3da48ec36bd5b6a97
Rexben <rexben.rb@gmail.com>
2022-01-22 21:41:38 +0100

    feat: add addTransactions reducer

--- src/store.ts
+++ src/store.ts
@@ -12,9 +12,10 @@ export const { actions, reducer } = createSlice({
   name: 'store',
   initialState,
   reducers: {
-    set: (state, action: PayloadAction<Partial<State>>) => ({ ...state, ...action.payload })
-    // TODO: Write this reducer
-    // addTransactions:
+    set: (state, action: PayloadAction<Partial<State>>) => ({ ...state, ...action.payload }),
+    addTransactions: (state, action: PayloadAction<Partial<State>>) => {
+      state.transactions = { ...state.transactions, ...action.payload.transactions };
+    }
   }
 });
 
