aabf4fc305d96dab8564af3888d887a3190b1361
Rexben <rexben.rb@gmail.com>
2022-01-22 21:40:10 +0100

    feat: add apis

--- src/hooks/useAPI.ts
+++ src/hooks/useAPI.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 import { toast } from 'react-toastify';
 
-import { Task } from '../types';
+import { Task, Transaction } from '../types';
 
 const useAPI = () => {
   const getTasks = useCallback(async (): Promise<Task[]> => {
@@ -28,8 +28,93 @@ const useAPI = () => {
     return [];
   }, []);
 
+  const authenticateUser = async ({ email, password }: { email: string; password: string }) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/authenticate`, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        body: JSON.stringify({
+          email,
+          password
+        })
+      });
+
+      if (response.status !== 200) {
+        toast(`API request failed`, { type: 'error' });
+
+        return;
+      }
+
+      return await response.json();
+    } catch (e) {
+      console.log(e);
+
+      toast(`API request failed`, { type: 'error' });
+    }
+
+    return;
+  };
+
+  const getTransactions = useCallback(
+    async (
+      page: number
+    ): Promise<{
+      [key: string]: Transaction;
+    }> => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/transactions?page=${page}`, {
+          method: 'GET',
+          mode: 'cors',
+          credentials: 'include'
+        });
+
+        if (response.status !== 200) {
+          toast(`API request failed`, { type: 'error' });
+
+          return {};
+        }
+
+        return await response.json();
+      } catch (e) {
+        console.log(e);
+
+        toast(`API request failed`, { type: 'error' });
+      }
+
+      return {};
+    },
+    []
+  );
+  const deleteTransactionByID = useCallback(async (id: string) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/transactions/delete/${id}`, {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include'
+      });
+
+      if (response.status !== 200) {
+        toast(`API request failed`, { type: 'error' });
+
+        return {};
+      }
+
+      return await response.json();
+    } catch (e) {
+      console.log(e);
+
+      toast(`API request failed`, { type: 'error' });
+    }
+
+    return {};
+  }, []);
+
   return {
-    getTasks
+    getTasks,
+    authenticateUser,
+    getTransactions,
+    deleteTransactionByID
   };
 };
 
